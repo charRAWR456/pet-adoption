c3255a67c4c238bf7b270fb8c66421754e44000f
Jamie Pittak, Miranda Keith and Tyler Wickline <jamie@>
2018-01-29 14:06:48 -0800

    add method to generate HTML for an animal object

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,14 @@ function Animal(name, species) {
   this.name = name;
   this.species = species;
   this.adopted = false;
+  this.id = Animal.prototype.id++;
+  this.picture = "img/kitten.jpg";
+  this.age = 3;
+  this.sex = "unknown";
+  this.type = "unknown";
 }
+
+Animal.prototype.id = 0;
 
 const animalTypes = {cat: "cat", dog: "dog", bird: "bird"};
 
@@ -30,14 +37,37 @@ function getTestAnimals(){
 }
 
 function getCardHTML(animal){
-  return string;
+  var output =
+  `<div class="animal-card" id="animal-${animal.id}">
+    <div class="card" name="front">
+      <img src="${animal.image} alt="Picture of the animal" class="card-img-top">
+      <div class="card-footer">
+        <h3>${animal.name} | Age ${animal.age}</h3>
+      </div>
+    </div>
+    <div class="card" name="back">
+      <div class="card-body">
+        <h5 class="card-title">About ${animal.name}</h5>
+        <ul>
+          <li>Age: ${animal.age}</li>
+          <li>Sex: ${animal.sex}</li>
+          <li>Type: ${animal.type}</li>
+        </ul>
+      </div>
+      <div class="card-footer">
+        <button type="button" name="button" class="btn btn-primary">Adopt ${animal.name}!</button>
+      </div>
+    </div>
+  </div>`;
+  return output;
 }
 
 $(function () {
   $(".card[name=back]").hide();
   $('#myTab li:last-child a').tab('show');
   var animalsList = getTestAnimals();
-
+  console.log(getCardHTML(animalsList[0]));
+  console.log("hi");
   $("#animal-0").find(".card[name=front]").click(function(){
     $(this).fadeOut(function(){
       $("#animal-0").find(".card[name=back]").fadeIn();
