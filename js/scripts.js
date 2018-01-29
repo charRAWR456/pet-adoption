e5218803aba2085cff16ec4ebdfe90fec110e96b
Jamie Pittak <charrawr456@gmail.com>
2018-01-29 11:11:43 -0800

    add function to populate each tab with the corresponding pet names.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,32 @@
+var animalArray = [
+  new Animal("Fido", "dog"),
+  new Animal("Poochy", "dog"),
+  new Animal("Bob", "dog"),
+  new Animal("Fluffums", "cat"),
+  new Animal("Feisty", "cat"),
+  new Animal("Crackerz", "bird"),
+  new Animal("Ted", "bird")
+];
+
+
+function populateTab(tabName) {
+  $(tabName+"List").text("");
+  var animals;
+  if (tabName === "home") {
+    animals = animalArray;
+  }
+  else {
+    animals = animalSort(animalArray, "species", tabName);
+    console.log(animals);
+  };
+
+  animals.forEach(function(animal){
+
+    $("#"+tabName+"List").append("<li>" + animal.name + "</li>");
+
+  });
+}
+
 function Animal(name, species) {
   this.name = name;
   this.species = species;
@@ -5,8 +34,8 @@ function Animal(name, species) {
 }
 
 function animalSort(arr, propertyName, propertyValue) {
-  propertyName = propertyName.toString();
-  propertyValue = propertyValue.toString();
+  var propertyName = propertyName.toString();
+  var propertyValue = propertyValue.toString();
 
   var result = arr.filter(function(item){
     var isRight = item[propertyName].toString() === propertyValue;
@@ -17,5 +46,12 @@ function animalSort(arr, propertyName, propertyValue) {
 }
 
 $(function () {
-  $('#myTab li:last-child a').tab('show')
+
+  $('#myTab li:last-child a').tab('show');
+
+  populateTab("home");
+  populateTab("cat");
+  populateTab("dog");
+  populateTab("bird");
+
 })
