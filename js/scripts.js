95d62cf67294706ba0560e6e77e9767cbb09a747
Miranda Keith and Tyler Wickline <tyler@>
2018-01-29 11:48:58 -0800

    Add partial pet card.

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,8 @@ function Animal(name, species) {
   this.adopted = false;
 }
 
+const animalTypes = {cat: "cat", dog: "dog", bird: "bird"};
+
 function animalSort(arr, propertyName, propertyValue) {
   propertyName = propertyName.toString();
   propertyValue = propertyValue.toString();
@@ -16,6 +18,36 @@ function animalSort(arr, propertyName, propertyValue) {
   return result;
 }
 
+function getTestAnimals(){
+  var animalsList = [];
+  animalsList.push(new Animal("Pixel", animalTypes.cat));
+  animalsList.push(new Animal("Fido", animalTypes.dog));
+  animalsList.push(new Animal("Fluffy", animalTypes.dog));
+  animalsList.push(new Animal("Pippin", animalTypes.dog));
+  animalsList.push(new Animal("Kazoo", animalTypes.bird));
+  animalsList.push(new Animal("Treble", animalTypes.bird));
+  return animalsList;
+}
+
+function getCardHTML(animal){
+  return string;
+}
+
 $(function () {
-  $('#myTab li:last-child a').tab('show')
-})
+  $(".card[name=back]").hide();
+  $('#myTab li:last-child a').tab('show');
+  var animalsList = getTestAnimals();
+
+  $("#animal-0").find(".card[name=front]").click(function(){
+    $(this).fadeOut(function(){
+      $("#animal-0").find(".card[name=back]").fadeIn();
+    });
+  });
+
+  $("#animal-0").find(".card[name=back]").click(function(){
+    $(this).fadeOut(function(){
+      $("#animal-0").find(".card[name=front]").fadeIn();
+    })
+  });
+
+});
